577daea975187583bcac3cb020ecab8da69a1e7b
svetlilaloli <72747194+svetlilaloli@users.noreply.github.com>
2021-04-05 08:25:35 +0100

    initial commit

---
+++ CustomComparator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomComparator
+{
+    class CustomComparator
+    {
+        static void Main()
+        {
+            int[] numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            CustomEvenOddComparer compare = new CustomEvenOddComparer();
+            Array.Sort(numbers, compare);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ CustomEvenOddComparer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace CustomComparator
+{
+    class CustomEvenOddComparer : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            if (x % 2 == 0 && y % 2 != 0)
+            {
+                return -1;
+            }
+            if (x % 2 == 0 && y % 2 == 0)
+            {
+                return x - y;
+            }
+            if (x % 2 != 0 && y % 2 != 0)
+            {
+                return x - y;
+            }
+            if (x % 2 != 0 && y % 2 == 0)
+            {
+                return 1;
+            }
+            
+            return 0;
+        }
+    }
+}
